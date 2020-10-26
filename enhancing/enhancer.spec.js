e5e75cd0d593c146c24898bd0ffc68543266ffe7
Natividad Alvarez <tivialvarez@gmail.com>
2020-10-26 16:45:06 -0400

    Wrote test for repair function

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,13 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe("repair", ()=>{
+    it("Should return an item with with a durability of 100", ()=>{
+        const item = {
+            name: "Sword",
+            durability: 30,
+            enchantment: 3
+        }
+        expect(enhancer.repair(item).durability).toBe(100);
+    });
+})
